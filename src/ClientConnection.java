b2b7ba11e2e6c4c02d3b612972c52b42e2c32d7a
d0rf3n <d0rf3n@gmail.com>
2013-12-09 11:43:45 +0100

    update to ClientConnection

--- src/ClientConnection.java
+++ src/ClientConnection.java
@@ -1,18 +1,18 @@
 import java.io.*;
 import java.net.Socket;
 
-public class Client {
+public class ClientConnection {
 
 	private Socket connectionToServer = null;
 	private PrintWriter outStream = null;
 	private BufferedReader inStream = null;
 
-	public Client(String serverAddress, int serverPort) throws IOException{
+	public ClientConnection(String serverAddress, int serverPort) throws IOException {
 		connectionToServer = new Socket(serverAddress, serverPort);
 
 		//The getOutputStream and getInputStream methods return byte streams
 		outStream = new PrintWriter(new OutputStreamWriter(connectionToServer.getOutputStream()));
-		inStream  = new BufferedReader(new InputStreamReader(connectionToServer.getInputStream()));
+		inStream = new BufferedReader(new InputStreamReader(connectionToServer.getInputStream()));
 	}
 
 	public void send(String sMsg) {
@@ -23,6 +23,22 @@ public class Client {
 	public String receive() {
 		String rMsg = null;
 
+		try {
+			rMsg = inStream.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		return rMsg;
+	}
+
+	public void close(){
+		try{
+			inStream.close();
+			outStream.close();
+			connectionToServer.close();
+		}catch(IOException e){
+			e.printStackTrace();
+		}
 	}
 }
