eb5169ec1ee9760b666884c2dfbfb5d42fb51c02
magkur <magnus.kurtto@gmail.com>
2013-12-09 11:52:23 +0100

    Updated serverConnection.java
    added interfaces and implemented methods

--- src/ServerConnection.java
+++ src/ServerConnection.java
@@ -1,4 +1,16 @@
 
-public class ServerHandler {
+public class ServerConnection implements Runnable, Observer{
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
