fc860233263a67013748872afa06269f2ed40696
d0rf3n <d0rf3n@gmail.com>
2013-12-09 12:08:06 +0100

    update to ChatGUI

--- src/ChatGUI.java
+++ src/ChatGUI.java
@@ -1,11 +1,32 @@
 import javax.swing.*;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Properties;
 
 public class ChatGUI extends JFrame {
 
-	private Properties prop;
+	private Properties properties;
+	private ClientConnection clientConnection;
 
 	public ChatGUI(){
+		properties = new Properties();
+		loadProperties();
+		connectToServer(properties.getProperty("address"), Integer.parseInt(properties.getProperty("port")));
+	}
 
+	public void connectToServer(String serverAddress, int port) {
+		try {
+			clientConnection = new ClientConnection(serverAddress, port);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void loadProperties() {
+		try {
+			properties.load(new FileReader("prop"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
