262feb1323087a14d70a7634ddcf1795960b2ee2
magkur <magnus.kurtto@gmail.com>
2013-12-09 11:44:37 +0100

    added methods to server.java
    init, waitforconneciton, close, registeroberver, unregisterobserver,
    notifyobservers

--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,52 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
 
 public class Server {
+	private ServerSocket serverSocket = null;
+	private Socket connectionToClient = null;
+	private ArrayList<Observer> observerList;
 
+	public void init(int port){
+		observerList = new ArrayList<Observer>();
+		try {
+			serverSocket = new ServerSocket(port);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void waitForConnection(){
+		while(true){
+			try {
+				connectionToClient = serverSocket.accept();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			Thread newThread = new Thread(new ServerHandler(connectionToClient));
+			Observer observer = (Observer) newThread;
+			registerObserver(observer);
+			newThread.start();
+		}
+	}
+
+	public void close(){
+
+	}
+
+	public void registerObserver(Observer ob){
+		observerList.add(ob);
+	}
+
+	public void unRegisterObserver(Observer ob){
+		observerList.remove(ob);
+	}
+
+	public void notifyObservers(){
+		for(Observer o : observerList){
+			o.update();
+		}
+	}
 }
