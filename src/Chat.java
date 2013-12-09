95864d4c2485b453caea1893d492d8935501fb17
d0rf3n <d0rf3n@gmail.com>
2013-12-09 12:59:32 +0100

    added Chat, ChatGUI

---
+++ src/Chat.java
@@ -0,0 +1,31 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Chat {
+
+	private Properties properties;
+	private ClientConnection clientConnection;
+
+	public Chat() {
+		properties = new Properties();
+		loadProperties();
+		connectToServer(properties.getProperty("address"), Integer.parseInt(properties.getProperty("port")));
+	}
+
+	public void connectToServer(String serverAddress, int port) {
+		try {
+			clientConnection = new ClientConnection(serverAddress, port);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void loadProperties() {
+		try {
+			properties.load(new FileReader("prop"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
--- src/ChatGUI.java
+++ src/ChatGUI.java
@@ -1,32 +1,8 @@
 import javax.swing.*;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Properties;
 
 public class ChatGUI extends JFrame {
 
-	private Properties properties;
-	private ClientConnection clientConnection;
+	public ChatGUI() {
 
-	public ChatGUI(){
-		properties = new Properties();
-		loadProperties();
-		connectToServer(properties.getProperty("address"), Integer.parseInt(properties.getProperty("port")));
-	}
-
-	public void connectToServer(String serverAddress, int port) {
-		try {
-			clientConnection = new ClientConnection(serverAddress, port);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void loadProperties() {
-		try {
-			properties.load(new FileReader("prop"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 }
