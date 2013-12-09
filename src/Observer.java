547aa61238d13d20bfae24e736ccf0c3a66e3941
magkur <magnus.kurtto@gmail.com>
2013-12-09 12:11:53 +0100

    Updated ServerConnection

--- src/Observer.java
+++ src/Observer.java
@@ -1,5 +1,5 @@
 
 public interface Observer {
 
-	public void update();
+	public void update(String message);
 }
--- src/ServerConnection.java
+++ src/ServerConnection.java
@@ -2,11 +2,19 @@
 public class ServerConnection implements Runnable, Observer{
 
 	@Override
-	public void update() {
-		// TODO Auto-generated method stub
+	public void update(String message) {
+		send(message);
 		
 	}
 
+	public void send(String message){
+		
+	}
+	
+	public void receive(String message){
+		
+	}
+	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
