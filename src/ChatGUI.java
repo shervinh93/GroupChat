995ba9b961d673a4d8a90b643d9ed075ca90472f
Johnny Thuland <johnnythuland@gmail.com>
2013-12-11 10:44:47 +0100

    updated chatgui

--- src/ChatGUI.java
+++ src/ChatGUI.java
@@ -1,88 +1,121 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 import javax.swing.*;
 
-public class ChatGUI extends JFrame{
+public class ChatGUI extends JFrame implements ActionListener{
 
-	String username, serverAddress, port;
+	private JButton button1 = new JButton("Send");
+	private JTextArea textArea = new JTextArea(30 ,28);
+	private JTextField textField = new JTextField("hejsan");
+	private String text;
+	private JPanel panel = new JPanel();
+	private JMenuBar menuBar = new JMenuBar();
+	private JMenu fileMenu = new JMenu("File");
+	private JMenuItem connect = new JMenuItem("Connect");
+	private JMenuItem disconnect = new JMenuItem("Disconnect");
+	private JMenuItem exit = new JMenuItem("Exit");
+	private JMenu helpMenu = new JMenu("Help");
+	private JMenuItem about = new JMenuItem("About");
 	
-//	JTextArea textArea = new JTextArea(30 ,28);
-//	JTextField textField = new JTextField("hejsan");
-//	String text;
+	Properties prop = new Properties();	
+	
+
+
+	public void createWindow(){
+//		fönster
+		 setResizable(false);
+		 setSize(500, 500);
+		 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 setTitle("GroupChat");
+		 setVisible(true);
+		 
+//		 arkivmeny
+		 setJMenuBar(menuBar);
+		 menuBar.add(fileMenu);
+		 fileMenu.add(connect);
+		 fileMenu.add(disconnect);
+		 fileMenu.add(exit);
+		 menuBar.add(helpMenu);
+		 helpMenu.add(about);
+     	 		        	
+//		 panel
+		 
+		 getContentPane().add(panel);
+		 textArea.setEditable(false);
+		 panel.add(textArea);
+		 panel.add(textField);
+		 panel.add(button1);
+	
+//		 actioncommand & actionlistener
+		 button1.setActionCommand("Send");
+		 button1.addActionListener(this); 
+		 connect.setActionCommand("connect");
+		 connect.addActionListener(this);
+		 
+		 
+	}
+	
+	public void writeProperties(String name, String port){
+		
+		try {
+			//set the properties value
+			prop.setProperty("name", name);
+			prop.setProperty("address", "123");
+			prop.setProperty("port", port);
+
+			//save properties to project root folder
+			prop.store(new FileOutputStream("prop"), null);
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public void appendText(){
+//		textArea.append("\n"+inText);
+		textArea.append(textField.getText()+"\n");
+	}
+	
+	public void sendText(){
+		//textField.getText();
+	}
+	
+
+	public void actionPerformed(ActionEvent arg0) {
+		if ("Send".equals(arg0.getActionCommand())){
+			textArea.append(textField.getText()+"\n");
+			System.out.println("test");
+       	}	       
+    	else if ("connect".equals(arg0.getActionCommand())) {
+         	String name = JOptionPane.showInputDialog("Name");
+    		String port = JOptionPane.showInputDialog("Port");
+    		writeProperties(name, port);
+    		System.out.println("connect??");
+        }
+	}
+//	String username, serverAddress, port;
 //	
-//	public void createWindow(){
-////		fönster
-//		
-//		 setResizable(false);
-//		 setSize(500, 500);
-//		 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-////		 arkivmeny
-//		 JMenuBar menuBar = new JMenuBar();
-//		 setJMenuBar(menuBar);
-//		 JMenu fileMenu = new JMenu("File");
-//		 menuBar.add(fileMenu);
-//		 JMenuItem connect = new JMenuItem("Connect");
-//		 JMenuItem disconnect = new JMenuItem("Disconnect");
-//		 JMenuItem exit = new JMenuItem("Exit");
-//		 fileMenu.add(connect);
-//		 fileMenu.add(disconnect);
-//		 fileMenu.add(exit);
-//		 JMenu helpMenu = new JMenu("Help");
-//		 menuBar.add(helpMenu);
-//	     JMenuItem about = new JMenuItem("About");
-//     	 helpMenu.add(about);
-//     	 		        	
-////		 panel
-//		 JPanel panel = new JPanel();
-//		 getContentPane().add(panel);
-//		 
-//		 textArea.setEditable(false);
-//		 panel.add(textArea);
-//		 
-//		 panel.add(textField);
-//		 
-//		 JButton button1 = new JButton("Send");
-//		 panel.add(button1);
-//		 button1.setActionCommand("Send");
-//		 button1.addActionListener(this);
-//		 
-//		 connect.addActionListener(this);
-//		 connect.setActionCommand("connect");
-//		 
-//		 setTitle("GroupChat");
-//		 setVisible(true);
+//	public void printMessage(String message) {
+//
+//	}
+//
+//	public String askForUsername() {
+//		String username = null;
+//
+//		while (username == null)
+//			username = JOptionPane.showInputDialog("Input username:");
+//
+//		this.username = username;
+//
+//		return username;
 //	}
 //	
-//	@Override
-//	public void actionPerformed(ActionEvent arg0) {
-//		if ("Send".equals(arg0.getActionCommand())){
-//			textArea.append(textField.getText()+"\n");			
-//       	}	       
-//    	else if ("connect".equals(arg0.getActionCommand())) {
-//         	String name = JOptionPane.showInputDialog("Name");
-//    		String port = JOptionPane.showInputDialog("Port");
-//        }
+//	public ChatGUI(String serverAddress, int port) {
+//		this.serverAddress = serverAddress;
+//		this.port = Integer.toString(port);
 //	}
-
-	
-	public void printMessage(String message) {
-
-	}
-
-	public String askForUsername() {
-		String username = null;
-
-		while (username == null)
-			username = JOptionPane.showInputDialog("Input username:");
-
-		this.username = username;
-
-		return username;
-	}
-	
-	public ChatGUI(String serverAddress, int port) {
-		this.serverAddress = serverAddress;
-		this.port = Integer.toString(port);
-	}
 }
